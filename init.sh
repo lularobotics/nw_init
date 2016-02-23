99f30d559288d9cb982f4bc1684cb4d4dd3b2397
Nathan Ratliff <ratliff.nathan@gmail.com>
2016-02-23 10:13:15 -0800

    Missing quotation mark in init.sh

--- init.sh
+++ init.sh
@@ -181,7 +181,7 @@ echo -e "# source <workspace>/devel/setup.bash"
 echo -e "# rosrun nw_mico_client run_basic_mico_demos"
 echo -e "#"
 echo -e "# --- When finished, shutdown with:"
-echo -e "# rosrun nw_motion_optimization riemo_mico_server_shutdown.sh
+echo -e "# rosrun nw_motion_optimization riemo_mico_server_shutdown.sh"
 echo -e "#"
 echo -e "###############################################################"
 }
